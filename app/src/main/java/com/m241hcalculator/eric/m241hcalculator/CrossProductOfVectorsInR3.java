575e9044915d01b4c786972c98fc70a856292a1f
Eric Liu <eric1932777@gmail.com>
2018-09-04 14:52:24 -0500

    Refine

--- app/src/main/java/com/m241hcalculator/eric/m241hcalculator/CrossProductOfVectorsInR3.java
+++ app/src/main/java/com/m241hcalculator/eric/m241hcalculator/CrossProductOfVectorsInR3.java
@@ -4,15 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class CrossProductOfVectorsInR3 extends AppCompatActivity {
 
     private static TextView ai, aj, ak, bi, bj, bk;
-    //private static int int_ai, int_aj, int_ak, int_bi, int_bj, int_bk;
-    private static double result;
     private static TextView vectors_textview[];
 
     @Override
@@ -22,24 +19,24 @@ public class CrossProductOfVectorsInR3 extends AppCompatActivity {
     }
 
     public void onClickCal(View v) {
-        ai = (TextView) findViewById(R.id.B_ai);
-        aj = (TextView) findViewById(R.id.B_aj);
-        ak = (TextView) findViewById(R.id.B_ak);
-        bi = (TextView) findViewById(R.id.B_bi);
-        bj = (TextView) findViewById(R.id.B_bj);
-        bk = (TextView) findViewById(R.id.B_bk);
+        ai = findViewById(R.id.B_ai);
+        aj = findViewById(R.id.B_aj);
+        ak = findViewById(R.id.B_ak);
+        bi = findViewById(R.id.B_bi);
+        bj = findViewById(R.id.B_bj);
+        bk = findViewById(R.id.B_bk);
         //没搞明白
         vectors_textview = new TextView[]{ai, aj, ak, bi, bj, bk};
 
-        for (int i = 0; i < vectors_textview.length; i++) {
-            if (vectors_textview[i] == null || vectors_textview[i].getText().toString().length() == 0) {
+        for (TextView i : vectors_textview) {
+            if (i == null || i.getText().toString().length() == 0) {
                 Log.d("onclick_logd", "return because of null/0");
-                ((View) vectors_textview[i]).requestFocus();
+                i.requestFocus();
                 return;
             }
-            for (int j = 0; j < vectors_textview[i].getText().toString().length(); j++) {
+            for (int j = 0; j < i.getText().toString().length(); j++) {
                 //check if ijks are only 0-9
-                if (vectors_textview[i].getText().toString().charAt(j) < 48 || vectors_textview[i].getText().toString().charAt(j) > 57) {
+                if (i.getText().toString().charAt(j) < 48 || i.getText().toString().charAt(j) > 57) {
                     Log.d("onclick_logd", "return because of not 0-9");
                     Toast.makeText(this, "Invalid ijks", Toast.LENGTH_SHORT).show();
                     return;
@@ -56,14 +53,18 @@ public class CrossProductOfVectorsInR3 extends AppCompatActivity {
                 Integer.parseInt(bj.getText().toString()),
                 Integer.parseInt(bk.getText().toString())};
 
-        result = (x[1] * x[5] - x[2] * x[4]) - (x[2] * x[3] - x[0] * x[5]) + (x[0] * x[4] - x[1] * x[3]);
+        double result = (x[1] * x[5] - x[2] * x[4]) - (x[2] * x[3] - x[0] * x[5]) + (x[0] * x[4] - x[1] * x[3]);
         ((TextView) findViewById(R.id.textViewB_result)).setText(String.valueOf(result));
 
         ((TextView) findViewById(R.id.textViewB_result_vector)).setText("<" + (x[1] * x[5] - x[2] * x[4]) + ", "
                 + (x[2] * x[3] - x[0] * x[5]) + ", "
                 + (x[0] * x[4] - x[1] * x[3]) + ">");
 
-        ((Button) findViewById(R.id.buttonBClear)).setVisibility(View.VISIBLE);
+        //禁止修改
+        findViewById(R.id.buttonBCal).setEnabled(false);
+        for (TextView y : vectors_textview) {
+            y.setEnabled(false);
+        }
     }
 
     public void onClickClear(View v) {
@@ -72,6 +73,12 @@ public class CrossProductOfVectorsInR3 extends AppCompatActivity {
         }
         ((TextView) findViewById(R.id.textViewB_result)).setText("");
         ((TextView) findViewById(R.id.textViewB_result_vector)).setText("");
-        ((Button) findViewById(R.id.buttonBClear)).setVisibility(View.INVISIBLE);
+        //if Cal is disabled
+        if (!findViewById(R.id.buttonBCal).isEnabled()) {
+            findViewById(R.id.buttonBCal).setEnabled(true);
+        }
+        for (TextView y : vectors_textview) {
+            y.setEnabled(true);
+        }
     }
 }
--- app/src/main/java/com/m241hcalculator/eric/m241hcalculator/mode_for_vectors_in_R3.java
+++ app/src/main/java/com/m241hcalculator/eric/m241hcalculator/mode_for_vectors_in_R3.java
@@ -33,25 +33,25 @@ public class mode_for_vectors_in_R3 extends AppCompatActivity {
 
         double result = Math.pow(Math.pow(int_ai, 2) + Math.pow(int_aj, 2) + Math.pow(int_ak, 2) + Math.pow(int_bi, 2) + Math.pow(int_bj, 2) + Math.pow(int_bk, 2), 0.5);
 
-        TextView text_result = (TextView) findViewById(R.id.result);
+        TextView text_result = findViewById(R.id.result);
         text_result.setText(String.valueOf(String.valueOf(result)));
     }
 
     public void review(View v) {
-        ai = (EditText) findViewById(R.id.vector_a_i);
-        aj = (EditText) findViewById(R.id.vector_a_j);
-        ak = (EditText) findViewById(R.id.vector_a_k);
-        bi = (EditText) findViewById(R.id.vector_b_i);
-        bj = (EditText) findViewById(R.id.vector_b_j);
-        bk = (EditText) findViewById(R.id.vector_b_k);
+        ai = findViewById(R.id.vector_a_i);
+        aj = findViewById(R.id.vector_a_j);
+        ak = findViewById(R.id.vector_a_k);
+        bi = findViewById(R.id.vector_b_i);
+        bj = findViewById(R.id.vector_b_j);
+        bk = findViewById(R.id.vector_b_k);
         EditText vectors[] = {ai, aj, ak, bi, bj, bk};
-        for (int i = 0; i < vectors.length; i++) {
-            if (vectors[i] == null || vectors[i].getText().toString().length() == 0) {
+        for (EditText i : vectors) {
+            if (i == null || i.getText().toString().length() == 0) {
                 Log.d("review_logd", "return because of null/0");
                 return;
             }
-            for (int j = 0; j < vectors[i].getText().toString().length(); j++) {
-                if (vectors[i].getText().toString().charAt(j) < 48 || vectors[i].getText().toString().charAt(j) > 57) {
+            for (int j = 0; j < i.getText().toString().length(); j++) {
+                if (i.getText().toString().charAt(j) < 48 || i.getText().toString().charAt(j) > 57) {
                     //not only 0-9
                     Log.d("review_logd", "return because of not 0-9");
                     Toast.makeText(this, "Invalid ijks", Toast.LENGTH_SHORT).show();
@@ -65,12 +65,12 @@ public class mode_for_vectors_in_R3 extends AppCompatActivity {
     }
 
     public void onClickClear(View v) {
-        ai = (EditText) findViewById(R.id.vector_a_i);
-        aj = (EditText) findViewById(R.id.vector_a_j);
-        ak = (EditText) findViewById(R.id.vector_a_k);
-        bi = (EditText) findViewById(R.id.vector_b_i);
-        bj = (EditText) findViewById(R.id.vector_b_j);
-        bk = (EditText) findViewById(R.id.vector_b_k);
+        ai = findViewById(R.id.vector_a_i);
+        aj = findViewById(R.id.vector_a_j);
+        ak = findViewById(R.id.vector_a_k);
+        bi = findViewById(R.id.vector_b_i);
+        bj = findViewById(R.id.vector_b_j);
+        bk = findViewById(R.id.vector_b_k);
         EditText vectors[] = {ai, aj, ak, bi, bj, bk};
         for (EditText x : vectors) {
             x.setText("");
